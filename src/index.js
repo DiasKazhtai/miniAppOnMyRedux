5ff1f66f7a248760ba65ea9d1c6ee5dc18e4a132
Dias Kazhtai <dkazhtai21@mail.ru>
2020-06-13 14:16:36 +0300

    finish app without redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './styles.css'
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const theme = document.getElementById('theme')
+const async = document.getElementById('async')
+
+let state = 0
+
+function render (){
+    counter.textContent = state.toString()
+}
+addBtn.addEventListener('click', () => {
+   state++
+    render()
+})
+
+subBtn.addEventListener('click', () => {
+    state--
+    render()
+ })
+
+ async.addEventListener('click', () => {
+     setTimeout(()=>{
+         state++
+         render()
+     }, 2000)
+ })
+
+ theme.addEventListener('click', () => {
+     document.body.classList.toggle('dark')
+ })
+
+render()
